e327709f7e12dfb4d37fb0f3cc61986b51215ea0
Dima <dima@sukharev.io>
2020-04-21 17:13:41 +0300

    fixed typo in binarySearch

--- challenges/binarySearch/index.js
+++ challenges/binarySearch/index.js
@@ -1,6 +1,13 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable no-multi-assign */
 
+/**
+ * Binary search algorithm implementation.
+ *
+ * @param {*} arr
+ * @param {*} search
+ * @returns positive index if value was finded & -1 if there was no value in the array
+ */
 export default function binarySearch(arr, search) {
   let left = 0;
   let right = arr.length - 1;
@@ -13,5 +20,5 @@ export default function binarySearch(arr, search) {
     else if (arr[mid] > search) right = mid;
     else if (arr[mid] < search) left = mid;
   }
-  return false; // if no search value is in array
+  return -1; // if no search value is in array
 }
--- challenges/binarySearch/index.test.js
+++ challenges/binarySearch/index.test.js
@@ -3,7 +3,7 @@ import binarySearch from "./index.js";
 describe("Binary Search", () => {
   test("Should return index of finded value in sorted array", () => {
     expect(binarySearch([1, 3, 5, 6, 7, 8, 9], 5)).toBe(2);
-    expect(binarySearch([1, 3, 5, 6, 7, 8, 9], 4)).toBeFalsy();
-    expect(binarySearch([1, 3, 5, 6, 7, 8, 9], 15)).toBeFalsy();
+    expect(binarySearch([1, 3, 5, 6, 7, 8, 9], 4)).toBe(-1);
+    expect(binarySearch([1, 3, 5, 6, 7, 8, 9], 15)).toBe(-1);
   });
 });
