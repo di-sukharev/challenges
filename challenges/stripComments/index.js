f7cc2fb4c2bf84186342e663ae518465bfa27546
Dima <dima@sukharev.io>
2020-05-14 19:32:32 +0300

    improved stripComments

--- challenges/stripComments/index.js
+++ challenges/stripComments/index.js
@@ -1,8 +1,5 @@
 export default function stripComments(input, markers) {
-  let result = input;
-  for (const marker of markers) {
-    const regexp = new RegExp(`\\s?(${marker.match(/[$^?]/) ? `\\${marker}` : marker}.*)`, "gim");
-    result = result.replace(regexp, "");
-  }
-  return result.trim();
+  const result = input;
+  const regexp = new RegExp(`\\s?[${markers.join("")}].*`, "gi");
+  return result.replace(regexp, "").trim();
 }
