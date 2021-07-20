f73174cd219143caab0dc8e662afa097415567a1
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2021-07-20 16:59:21 +0300

    merge sort ready

--- challenges/sorting/mergeSort/index.js
+++ challenges/sorting/mergeSort/index.js
@@ -17,28 +17,27 @@
  */
 
 function mergeSort(arr) {
-  // base-case (stop recursion)
   if (arr.length <= 1) return arr;
 
   const mid = Math.floor(arr.length / 2);
   const left = arr.slice(0, mid);
-  const right = arr.slice(mid);
+  const right = arr.slice(mid, arr.length);
 
   return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
-  let i = 0; // left array pointer
-  let j = 0; // right array pointer
+  let i = 0;
+  let j = 0;
   const sorted = [];
 
   while (i < left.length && j < right.length) {
     if (left[i] < right[j]) {
       sorted.push(left[i]);
-      i++; // move left array pointer
+      i++;
     } else {
       sorted.push(right[j]);
-      j++; // move right array pointer
+      j++;
     }
   }
 
