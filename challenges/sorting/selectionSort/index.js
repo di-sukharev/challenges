07975254cd116a70f221dfd222a85a886c607f61
dmitry.sukharev <dmitry.sukharev@alibaba-inc.com>
2021-07-20 14:51:30 +0300

    selection sort ready

--- challenges/sorting/selectionSort/index.js
+++ challenges/sorting/selectionSort/index.js
@@ -2,32 +2,36 @@
  * Selection sort algorithm implementation.
  * O(N^2) time complexity & O(1) memory complexity.
  *
+ * Pseudo-code:
+ * repeat (numOfElements - 1) times
+ *    set the first unsorted element as the minimum
+ *    for each of the unsorted elements
+ *      if element < currentMinimum
+ *        set element as new minimum
+ *    swap minimum with first unsorted position
+ *
  * @param {*} arr
  * @returns {*} arr (sorted array)
  */
 
 function selectionSort(arr) {
-  const sorted = arr;
+  const sorted = [...arr];
 
-  let firstUnsortedIndex = 0;
+  for (let i = 0; i < sorted.length; i++) {
+    let minIndex = i;
 
-  // while elements are unsorted
-  while (firstUnsortedIndex < sorted.length - 1) {
-    let minValueIndex = firstUnsortedIndex;
-
-    // find the minimum value of all unsorted elements
-    for (let i = firstUnsortedIndex + 1; i < sorted.length; i++) {
-      if (sorted[i] < sorted[minValueIndex]) minValueIndex = i;
+    for (let j = i + 1; j < sorted.length; j++) {
+      if (sorted[j] < sorted[minIndex]) {
+        minIndex = j;
+      }
     }
 
-    // start swap of first unsorted element with the minimum value
-    const save = sorted[minValueIndex];
-    sorted[minValueIndex] = sorted[firstUnsortedIndex];
-    sorted[firstUnsortedIndex] = save;
-    // end swap
-
-    firstUnsortedIndex++;
+    const temp = sorted[i];
+    sorted[i] = sorted[minIndex];
+    sorted[minIndex] = temp;
   }
+
+  console.log({ arr, sorted });
 
   return sorted;
 }
